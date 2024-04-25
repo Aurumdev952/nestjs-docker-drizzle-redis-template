d70be09a89e33528675c3aad8689620175a4b186
Aurumdev952 <benjamugi20072@gmail.com>
2024-04-25 11:28:00 +0200

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app/configration.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig: AppConfigService = app.get(AppConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('')
+    .setDescription('Backend API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
